54f9e81db37c2e8f85244cc426ed05dd59fb4e4a
Eugene-Manjaro <eugene.dyachkov27@gmail.com>
2023-03-04 19:23:49 +0300

    Pre-relese

--- server/server.py
+++ server/server.py
@@ -1,17 +1,47 @@
 import asyncio
 import websockets
-
-async def command_processing(comand: str):
-    await print(comand)
+import pyautogui as pg
+def command_processing(comand: str):
+    if comand =="hello!":
+        pass
+    elif comand =="power":
+        pg.hotkey('ctrlleft', 'altleft', 'del')
+    elif comand =="out":
+        pg.press('esc')
+    elif comand =="brikUP":
+        pg.hotkey('fn','f5')
+    elif comand =="volumeUp":
+        pg.hotkey('fn','volumeup')
+    elif comand =="brigDown":
+        pg.hotkey('fn','f4')
+    elif comand =="volumeDown ":
+        pg.hotkey('fn','volumedown')
+    elif comand =="arrUp":
+        pg.press('up')
+    elif comand =="arrLeft":
+        pg.press('left')
+    elif comand =="arrPause":
+        pg.press('space')
+    elif comand =="arrRight":
+        pg.press('right')
+    elif comand =="arrDown":
+        pg.press('down')
+    elif comand =="enter":
+        pass
+    elif comand =="window":
+        pg.press('win')
 
 async def new_client(client_socket, path):
     print("New client")
     while True:
-        comand = await client_socket.recv()
-        await command_processing(comand)
+        try:
+            comand = await client_socket.recv()
+            await command_processing(comand)
+        except:
+            pass
 
 async def start_server():
-    await websockets.serve(new_client, "localhost", 8855)
+    await websockets.serve(new_client, "192.168.1.107", 8855)
 
 if __name__ == "__main__":
     evet_loop = asyncio.get_event_loop()
--- site/index.js
+++ site/index.js
@@ -1,6 +1,76 @@
 document.addEventListener('DOMContentLoaded',function(){
-    var connection = new WebSocket('ws://localhost:8855/');
-    console.log("its OK");
+    const power = document.querySelector("[name=button_power]")
+    const buttonOut = document.querySelector("[name=button_out]")
+    const brigUp = document.querySelector("[name=button_brig_up]")
+    const volumeUp = document.querySelector("[name=button_volume_up]")
+    const brigDown = document.querySelector("[name=brig_down]")
+    const volumeDown = document.querySelector("[name=button_volume_down]")
+    const arrUp = document.querySelector("[name=button_arr_up]")
+    const arrLeft = document.querySelector("[name=button_arr_left]")
+    const arrPause = document.querySelector("[name=button_arr_enter]")
+    const arrRight = document.querySelector("[name=button_arr_right]")
+    const arrDown = document.querySelector("[name=button_arr_down]")
+    const enter = document.querySelector("[name=button_enter]")
+    const window = document.querySelector("[name=button_window]")
+
+    var connection = new WebSocket('ws://192.168.1.107:8855/');
+
+    connection.onopen = () =>{
+        console.log("its OK");
+        connection.send("hello!");
+    }
+
+    power.onclick = () => {
+        connection.send("power")
+    }
+
+    buttonOut.onclick = () => {
+        connection.send("out")
+    }
+
+    brigUp.onclick = () => {
+        connection.send("brikUP")
+    }
+
+    volumeUp.onclick = () => {
+        connection.send("volumeUp")
+    }
+
+    brigDown.onclick = () => {
+        connection.send("brigDown")
+    }
+
+    volumeDown.onclick = () => {
+        connection.send("volumeDown")
+    }
+
+    arrUp.onclick = () => {
+        connection.send("arrUp")
+    }
+
+    arrLeft.onclick = () => {
+        connection.send("arrLeft")
+    }
+
+    arrPause.onclick = () => {
+        connection.send("arrPause")
+    }
+
+    arrRight.onclick = () => {
+        connection.send("arrRight")
+    }
+
+    arrDown.onclick = () => {
+        connection.send("arrDown")
+    }
+
+    enter.onclick = () => {
+        connection.send("enter")
+    }
+
+    window.onclick = () => {
+        connection.send("window")
+    }
 
 
 }, false);
